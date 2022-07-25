156900ed7dae55d11f5e3855a826e1cb10fe1cf8
Hexadecimalz <root@f3281a04bd2c.mylabserver.com>
2022-07-25 22:15:52 +0000

    Add first module code and map module to class

---
+++ manifests/install.pp
@@ -0,0 +1,11 @@
+# @summary Install the base apache package 
+#
+# A description of what this class does
+#
+# @example
+#   include apache::install
+class apache::install {
+  package { 'httpd':
+    ensure => 'present',
+  } 
+}
